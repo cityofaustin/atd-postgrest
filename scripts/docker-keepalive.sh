c62d8ec758e0e0bb6a317954946cb135d365febf
John Clary <john.clary@austintexas.gov>
2022-08-08 12:56:33 -0400

    compose down before up again

--- scripts/docker-keepalive.sh
+++ scripts/docker-keepalive.sh
@@ -2,6 +2,8 @@
 if [ ! "$(sudo docker ps | grep atd-postgrest_haproxy_1)" ]; then
     # container is not running - prune all stopped containers
     sudo docker container prune --force
+    # stop docker-compose (in case postgrest services are running)
+    sudo docker-compose --file /home/ec2-user/atd-postgrest/docker-compose.yaml --env-file /home/ec2-user/atd-postgrest/env down
     # start docker-compose
     sudo docker-compose --file /home/ec2-user/atd-postgrest/docker-compose.yaml --env-file /home/ec2-user/atd-postgrest/env up -d
 fi
